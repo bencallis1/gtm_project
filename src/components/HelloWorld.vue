cc67e5fdf591b7082ad40447ce9eae1638b0de2d
Ben Callis <bencallis1@gmail.com>
2019-03-07 12:27:38 -0700

    more html

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -41,7 +41,7 @@
           <div class="cover__image_box--10">
             <div class="cover__button_box--10">
               <button data-popup-target="video" type="button" class="cover__button--10 button button--blue button--blue-shadow button--round button--play-icon js-open-popup"></button>
-            </div><img src="img/pictures/pic--keyboard-image.png" class="cover__image">
+            </div><img src="../img/pictures/pic--keyboard-image.png" class="cover__image">
           </div>
         </div>
         <div class="cover__features_box">
@@ -61,6 +61,178 @@
           </div>
         </div>
       </div>
+    </div>
+    <div style="background-image: url(../img/bg/bg--backgrounded-macbook.jpg)" class="about about--06">
+      <div class="container about__container--06">
+        <div class="about__title_box--06">
+          <h2 class="about__title about__title--06">The new generation<br>UI kit for professionals</h2><span class="about__description--06">Simplicity is the keynote of all true elegance. A great man is always willing to be very little.</span>
+        </div>
+        <div class="row about__row--06">
+          <div class="col-md-6 col-lg-3 about__feature_item--06">
+            <div class="about__item_icon--06 about__feature_icon--heart-icon--blue"></div>
+            <h3 class="about__feature_item_title--06">flexible code</h3><span class="about__item_text--06">You are not permitted to live and die as a non-entity because you have encountered the greatness that is associated with Christ.</span><a href="#" class="about__item_link">learn more</a>
+          </div>
+          <div class="col-md-6 col-lg-3 about__feature_item--06">
+            <div class="about__item_icon--06 about__feature_icon--zip-icon--blue"></div>
+            <h3 class="about__feature_item_title--06">easy grid</h3><span class="about__item_text--06">The male orientation of classical Athens was inseparable from genius. Athens were great despite but because of its men.</span><a href="#" class="about__item_link">learn more</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- == END OF ABOUT 06 ==-->
+    <!-- == START OF CONTENTS 01 ==-->
+    <div class="contents contents--01">
+      <div class="container contents__container--01">
+        <div class="contents__image_box--01"><img src="../img/pictures/pic--hovering-phones.png" class="contents__image--01"></div>
+        <div class="contents__content_box--01">
+          <h3 class="contents__title--01">Modern technologies</h3><span class="contents__text--01">A man's life is interesting primarily when he has totaly failed down. I well know it. For its a sign that he well tried to surpass himself..</span><a href="#" class="button button--icon-apple--black button--white button--gray-shadow">download</a>
+        </div>
+      </div>
+    </div>
+    <!-- == END OF CONTENTS 01 ==-->
+    <!-- == START OF FEATURES 05 ==-->
+    <div class="features features--05">
+      <div class="container">
+        <div class="features__title_box features__title_box--05">
+          <h2 class="features__title--05">Always giving the best</h2>
+        </div>
+      </div>
+      <div class="container">
+        <div class="row">
+          <div class="col-xs-12 col-md-6 col-lg-4 features__item features__item--05">
+            <div class="features__item_icon_circle features__item_icon_circle--05 features__item_icon_circle--trophy-icon--blue"></div>
+            <h3 class="features__item_title">Excellent design</h3><span class="features__item_text">Until you're finally ready to look foolish, you'll never have the possibility of being great.</span>
+          </div>
+          <div class="col-xs-12 col-md-6 col-lg-4 features__item features__item--05">
+            <div class="features__item_icon_circle features__item_icon_circle--05 features__item_icon_circle--magnifier-icon--blue"></div>
+            <h3 class="features__item_title">SEO Friendly</h3><span class="features__item_text">When our little bit is combined with His good greatness, the all team is truly unbeatable.</span>
+          </div>
+          <div class="col-xs-12 col-md-6 col-lg-4 features__item features__item--05">
+            <div class="features__item_icon_circle features__item_icon_circle--05 features__item_icon_circle--zip-icon--blue"></div>
+            <h3 class="features__item_title">Fastest launch</h3><span class="features__item_text">Doing what needs to be done may not make you happy, but it will make you very great.</span>
+          </div>
+          <div class="col-xs-12 col-md-6 col-lg-4 features__item features__item--05">
+            <div class="features__item_icon_circle features__item_icon_circle--05 features__item_icon_circle--stack-icon--blue"></div>
+            <h3 class="features__item_title">Fully Responsive</h3><span class="features__item_text">Who can feel the mineral and harmony of a believer if he has the density of the seeker.</span>
+          </div>
+          <div class="col-xs-12 col-md-6 col-lg-4 features__item features__item--05">
+            <div class="features__item_icon_circle features__item_icon_circle--05 features__item_icon_circle--star-icon--blue"></div>
+            <h3 class="features__item_title">free updates</h3><span class="features__item_text">Always be a first rate version of yourself and not a second rate version of someone else.</span>
+          </div>
+          <div class="col-xs-12 col-md-6 col-lg-4 features__item features__item--05">
+            <div class="features__item_icon_circle features__item_icon_circle--05 features__item_icon_circle--speedometer-icon--blue"></div>
+            <h3 class="features__item_title">Lightning speed</h3><span class="features__item_text">You know what's worse than not working hard? Working hard in the wrong direction.</span>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- == END OF FEATURES 05 ==-->
+    <!-- == START OF call_to_action 01 ==-->
+    <div style="background-image: url(../img/bg/bg--magic-mouse.jpg)" class="call_to_action call_to_action--01">
+      <div class="container">
+        <h2 class="call_to_action__title call_to_action__title--01">Become first</h2><span class="call_to_action__text call_to_action__text--01">Discover classic and brand new styles in a variety of shapes, colours and materials, from signature Frame to the latest teardrop inspired watches.</span>
+        <form action="/path/to/api" method="POST" class="call_to_action__form">
+          <div class="call_to_action__input_box--01">
+            <input type="email" placeholder="Email" required class="input input--dark call_to_action__input--01">
+            <button type="submit" class="button button--white call_to_action__button--01">send</button>
+          </div>
+        </form>
+      </div>
+    </div>
+    <!-- == END OF call_to_action 01 ==-->
+    <!-- == START OF PRICES 04 ==-->
+    <div class="prices prices--04">
+      <div class="container-fluid">
+        <div class="prices__title_box">
+          <h2 class="prices__title">The Best Chance</h2><span class="prices__description">When we are connected to the source, we sure will not be afraid of any task set before all of us.</span>
+        </div>
+        <div class="prices__items_box">
+          <div class="prices__item prices__item--04">
+            <h3 class="prices__item__title">basic</h3><span class="prices__item__price">$ 10</span><span class="prices__item__price_info">per month</span>
+            <ul class="prices__item__features_list">
+              <li class="prices__item__features_list__item">10 GB Disk Space</li>
+              <li class="prices__item__features_list__item">5 Sub Domains</li>
+              <li class="prices__item__features_list__item">5 Databases</li>
+              <li class="prices__item__features_list__item">5 Users</li>
+            </ul><a href="#" class="button button--border-fat button--border-blue">get started</a>
+          </div>
+          <div class="prices__item prices__item--04 prices__item--accent--04">
+            <h3 class="prices__item__title prices__item__title--white">medium</h3><span class="prices__item__price prices__item__price--white">$ 19</span><span class="prices__item__price_info prices__item__price_info--white">per month</span>
+            <ul class="prices__item__features_list">
+              <li class="prices__item__features_list__item prices__item__features_list__item--white prices__item__features_list__item--gray-tick">100 GB Disk Space</li>
+              <li class="prices__item__features_list__item prices__item__features_list__item--white prices__item__features_list__item--gray-tick">15 Sub Domains</li>
+              <li class="prices__item__features_list__item prices__item__features_list__item--white prices__item__features_list__item--gray-tick">15 Databases</li>
+              <li class="prices__item__features_list__item prices__item__features_list__item--white prices__item__features_list__item--gray-tick">20 Users</li>
+            </ul><a href="#" class="button button--white button--black-shadow">try now</a>
+          </div>
+          <div class="prices__item prices__item--04">
+            <h3 class="prices__item__title">enterprise</h3><span class="prices__item__price">$ 39</span><span class="prices__item__price_info">per month</span>
+            <ul class="prices__item__features_list">
+              <li class="prices__item__features_list__item">500 GB Disk Space</li>
+              <li class="prices__item__features_list__item">25 Sub Domains</li>
+              <li class="prices__item__features_list__item">25 Databases</li>
+              <li class="prices__item__features_list__item">Unlimited Users</li>
+            </ul><a href="#" class="button button--border-fat button--border-blue">get started</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- == END OF PRICES 04 ==-->
+    <!-- == START OF TESTIMONIALS 05 ==-->
+    <div class="testimonials testimonials--05">
+      <div class="container">
+        <h2 class="testimonials__title testimonials__title--05">Our clients say</h2><span class="testimonials__description">When I only begin to read, I forget I'm on this world. It lifts me on wings with high thoughts.</span>
+      </div>
+      <div class="testimonials__container--05">
+        <div class="testimonials__arrows_box testimonials__arrows_box--05">
+          <button type="button" class="testimonials__arrow testimonials__arrow--left testimonials__arrow--05 testimonials__arrow--05--left js-testimonials-prev-arrow--05"></button>
+          <button type="button" class="testimonials__arrow testimonials__arrow--right testimonials__arrow--05 testimonials__arrow--05--right js-testimonials-next-arrow--05"></button>
+        </div>
+        <div data-slick="{&quot;slidesToShow&quot;: 3, &quot;slidesToScroll&quot;: 1, &quot;fade&quot;: false, &quot;cssEase&quot;: &quot;linear&quot;, &quot;prevArrow&quot; : &quot;.js-testimonials-prev-arrow--05&quot;, &quot;nextArrow&quot;: &quot;.js-testimonials-next-arrow--05&quot;, &quot;centerMode&quot;: true, &quot;infinite&quot;: true, &quot;focusOnSelect&quot;: true, &quot;responsive&quot;: [{&quot;breakpoint&quot;: 900, &quot;settings&quot;: {&quot;slidesToShow&quot;: 2}}, {&quot;breakpoint&quot;: 650, &quot;settings&quot;: {&quot;slidesToShow&quot;: 1}}]}" class="testimonials__box testimonials__box--05 js-slider">
+          <div class="testimonials__item testimonials__item--05"><span class="testimonials__item_text testimonials__item_text--05">"Thanks to Phoenix UI kit we save tons of time daily! Every new promo page is based on this tool. No need to code anymore!"</span>
+            <div style="background-image:url(../img/pictures/pic--ive.png)" class="testimonials__item_photo"></div><span class="testimonials__item_name">Jonathan Ive</span><span class="testimonials__item_job">Apple</span>
+          </div>
+          <div class="testimonials__item testimonials__item--05"><span class="testimonials__item_text testimonials__item_text--05">"A reason to love Phoenix is its simplicity. Every component is very easy yet stylish. I don't not need to do any routine."</span>
+            <div style="background-image:url(../img/pictures/pic--dustin.jpg)" class="testimonials__item_photo"></div><span class="testimonials__item_name">Dustin Senos</span><span class="testimonials__item_job">Medium</span>
+          </div>
+          <div class="testimonials__item testimonials__item--05"><span class="testimonials__item_text testimonials__item_text--05">"There are lots of design UI kits, some of them has HTML5 files. But only Phoenix offers Jade + SCSS version for the top customization level."</span>
+            <div style="background-image:url(../img/pictures/pic--dave.png)" class="testimonials__item_photo"></div><span class="testimonials__item_name">Dave Gamache</span><span class="testimonials__item_job">Twitter</span>
+          </div>
+          <div class="testimonials__item testimonials__item--05"><span class="testimonials__item_text testimonials__item_text--05">"My developers can create awesome websites super fast. All new projects are 100% responsible and retina-ready. Love!"</span>
+            <div style="background-image:url(../img/pictures/pic--tobias.jpg)" class="testimonials__item_photo"></div><span class="testimonials__item_name">Tobias van Schneider</span><span class="testimonials__item_job">Semplice</span>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- == END OF TESTIMONIALS 05 ==-->
+    <!-- == START OF FOOTER 04 ==-->
+    <footer class="footer footer--04">
+      <div class="footer__main_box--04">
+        <div class="container footer__container"><a href="#" class="footer__logo_box footer__logo_box--04"><img src="../img/etc/logo--circle--white.svg" class="footer__logo"></a>
+          <ul class="footer__nav footer__nav--04">
+            <li class="footer__nav__item footer__nav__item--04"><a href="#" class="footer__nav__link footer__nav__link--04">about</a></li>
+            <li class="footer__nav__item footer__nav__item--04"><a href="#" class="footer__nav__link footer__nav__link--04">tour</a></li>
+            <li class="footer__nav__item footer__nav__item--04"><a href="#" class="footer__nav__link footer__nav__link--04">pricing</a></li>
+            <li class="footer__nav__item footer__nav__item--04"><a href="#" class="footer__nav__link footer__nav__link--04">blog</a></li>
+            <li class="footer__nav__item footer__nav__item--04"><a href="#" class="footer__nav__link footer__nav__link--04">contact</a></li>
+          </ul>
+          <ul class="social footer__social--04 footer__social_box">
+            <li class="social__item social__item--left_margin"><a href="#" title="Say hello in Twitter!" class="social__link social__link--twitter"></a></li>
+            <li class="social__item social__item--left_margin"><a href="#" title="Say hello in Facebook!" class="social__link social__link--facebook"></a></li>
+            <li class="social__item social__item--left_margin"><a href="#" title="Say hello in G+!" class="social__link social__link--g_plus"></a></li>
+          </ul>
+        </div>
+      </div>
+      <div class="footer__description_box--04">
+        <div class="container footer__container"><span class="footer__description footer__description--04">Our UI/UX researches helped to figure out that the perfect way to charm audience is to keep things clean and simple as much as possible.</span><span class="footer__copyright footer__copyright--04">&copy; UI-market.com, 2016</span></div>
+      </div>
+    </footer>
+    <!-- == END OF FOOTER 04 ==-->
+    <!-- == START OF VIDEO POPUP COMPONENT ==-->
+    <!-- == START OF VIDEO POPUP ==-->
+    <div data-popup-name="video" class="popup popup--video js-popup">
+      <button title="close" class="popup__close_btn js-close-all-popups"></button>
+      <iframe src="https://www.youtube.com/embed/9ZfN87gSjvI" frameborder="0" allowfullscreen class="popup__video_frame"></iframe>
     </div>
   </div>
 </template>
